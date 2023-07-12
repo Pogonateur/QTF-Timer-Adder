4bfcf02e00777ddee7ff3a99756cd75e666ae7d0
Pogonateur <didier.28071999@hotmail.fr>
2023-07-12 23:55:13 +0200

    Add project files.

---
+++ QTF Timer Adder/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace QTF_Timer_Adder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.entry1 = new System.Windows.Forms.TextBox();
+            this.entry2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // entry1
+            // 
+            this.entry1.Location = new System.Drawing.Point(65, 64);
+            this.entry1.Name = "entry1";
+            this.entry1.Size = new System.Drawing.Size(110, 20);
+            this.entry1.TabIndex = 0;
+            // 
+            // entry2
+            // 
+            this.entry2.Location = new System.Drawing.Point(222, 64);
+            this.entry2.Name = "entry2";
+            this.entry2.Size = new System.Drawing.Size(100, 20);
+            this.entry2.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(369, 64);
+            this.result.Name = "result";
+            this.result.ReadOnly = true;
+            this.result.Size = new System.Drawing.Size(108, 20);
+            this.result.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(539, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Format must be x:xx.xxx and nothing else (separators can be spaces, dots or colon" +
+    "s between the x\'s)";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(158, 104);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 6;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 177);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.entry2);
+            this.Controls.Add(this.entry1);
+            this.Name = "Form1";
+            this.Text = "QTF Timer Adder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox entry1;
+        private System.Windows.Forms.TextBox entry2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Calculate;
+    }
+}
+
---
+++ QTF Timer Adder/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QTF_Timer_Adder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            if (checkEntries())
+            {
+                result.Text = addEntries(entry1.Text, entry2.Text);
+            }
+        }
+
+        private bool checkEntries()
+        {
+            string format = "\\d(:|\\.| )\\d\\d(:|\\.| )\\d\\d\\d";
+            Regex rgex = new Regex(format);
+            if (rgex.IsMatch(entry1.Text) && rgex.IsMatch(entry2.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string addEntries(string e1, string e2)
+        {
+            int totalmsE1 = 0;
+            int totalmsE2 = 0;
+
+            char[] delimiterChars = { ':', '.', ' ' };
+            string[] wordsE1 = e1.Split(delimiterChars);
+            string[] wordsE2 = e2.Split(delimiterChars);
+
+            int minE1 = Int32.Parse(wordsE1[0]);
+            int minE2 = Int32.Parse(wordsE2[0]);
+            int secE1 = Int32.Parse(wordsE1[1]);
+            int secE2 = Int32.Parse(wordsE2[1]);
+            int msE1 = Int32.Parse(wordsE1[2]);
+            int msE2 = Int32.Parse(wordsE2[2]);
+
+            totalmsE1 = (minE1 * 60 * 1000) + (secE1 * 1000) + (msE1);
+            totalmsE2 = (minE2 * 60 * 1000) + (secE2 * 1000) + (msE2);
+
+            int msResult = 0;
+            int secResult = 0;
+            int minResult = 0;
+
+            msResult = (totalmsE1 % 1000) + (totalmsE2 % 1000);
+            if (msResult/1000>=1)
+            {
+                msResult = msResult % 1000;
+                secResult += 1;
+            }
+
+            secResult += secE1 + secE2;
+            if (secResult/60.0>=1)
+            {
+                secResult -= 60;
+                minResult += 1;
+            }
+
+            minResult += minE1 + minE2;
+
+            string msString = msResult.ToString();
+            string secString = secResult.ToString();
+
+            if (msResult-100<0)
+            {
+                msString = "0" + msString;
+                if (msResult - 10 < 0)
+                {
+                    msString = "0" + msString;
+                }
+            }
+            if (secResult - 10 < 0)
+            {
+                secString = "0" + secString;
+            }
+
+            string res = "" + minResult + ":" + secString + "." + msString;
+
+            return res;
+        }
+    }
+}
